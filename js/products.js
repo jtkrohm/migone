47445984f9ce1d1b7c9753bf171563c78e1aa70c
jtkrohm <jtkrohm@github.com>
2020-07-21 21:00:48 -0400

    update products.html and ./js/products.js

--- js/products.js
+++ js/products.js
@@ -1,13 +1,12 @@
-let productList = [
+let prodList = [
 "Bananas", "Bluggoes", "Breadfruit", "Butternut Squash", 
 "Cabbages", "Callaloo", "Cantiloupe", "Carrots", 
 "Celery", "Chive & thyme", "Christophene", "Coconut", 
 "Coconut Water", "Cucumbers", "Dasheen", "Eggs", 
 "Ginger", "Honey", "Kale", "Lettuce", "Limes", "Nutmegs", 
 "Ochroes", "Oranges", "Patchoi", "Parsley", "Pinapples", 
-"Plantains", "String Beans", "Sea Moss", "Seasoning", 
-"Peppers", "Sweet Potatoes", "Tomatoes","Watermelon", "Whole Chicken", 
-"Yam" ];
+"Plantains", "String Beans", "Sea Moss", "Seasoning Peppers", "Sweet Potatoes", "Tomatoes",
+"Watermelon", "Whole Chicken", "Yam" ]; 
 
 /* {"item":"Bananas","picture":"path/to/picture","vendor":[{"name":"price"}]},*/
   
@@ -39,17 +38,16 @@ let prodJSON = '{"product":[{"item":"Bananas","picture":"path/to/picture",
  {"item":"Patchoi","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
  {"item":"Parsley","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
  {"item":"Pinapples","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
- {"item":"Butternut Squash","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
- {"item":"Cabbages","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
- {"item":"Callaloo","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
- {"item":"Cantiloupe","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
- {"item":"Carrots","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
- {"item":"Celery","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
- {"item":"Chive & thyme","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
- {"item":"Christophene","picture":"path/to/picture","vendor":[{"name":"John","price":20}]},
- 
-]};
-
+ {"item":"Plantains","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
+ {"item":"String Beans","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
+ {"item":"Sea Moss","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
+ {"item":"Seasoning  peppers","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
+ {"item":"Sweet Potatoes","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
+ {"item":"Tomatoes","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
+ {"item":"Watermelons","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
+ {"item":"Whole Chicken","picture":"path/to/picture","vendor":[{"name":"John","price":20}]}, 
+ {"item":"Yam","picture":"path/to/picture","vendor":[{"name":"John","price":20}]},
+ ]};
 let prodObj = JSON.parse(prodJSON);
 
 document.getElementById("products").innerHTML= prodObj.product.vendor.name;
