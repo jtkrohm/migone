c94c46edf44b8d438c73d8b3d8720fcd8049560a
jtkrohm <59145535+jtkrohm@users.noreply.github.com>
2020-08-04 21:35:31 -0400

    Update load_vendors.py

--- load_vendors.py
+++ load_vendors.py
@@ -14,22 +14,29 @@ html = " "
 html0 = " "
 html1 = " "
 html2 = " "
+html3 = " "
 
 
 for vendor in vendors["vendor"]:
        html1 =  f'''
-			        <div class="products-center"> 
-			        <article class="product">
-					<div class="img-container">
-						<img src="./images/pexels/eggs_tray.webp" alt="" class="product-img">
-						<button class="bag-btn" data-id="1"><a href="tel: +14734091240"> Call Now </a> </button>
-					</div>
-					<h2> Poultry Vendor @ St. Paul's </h2>
-					<h5> eggs @ $20 per tray</h5>
-					<h5> eggs @ $10 per dozen </h5>
+                                <article class="product">
+					<h2> Poultry Farmer </h2>
+					<br>
+					<div class="img-container-prod">
+						<img src="./images/pexels/eggs_dozen.webp" alt="" class="product-img">
+						</div>
+					<div class="scrollproducts"> '''
+       for item in vendor["item"]:
+                    html2 = f'''
+					    <div>
+						<h5> Eggs</h5>
+						<h5> $10 per dozen </h5>
+  						<h5> <a href="tel: +0"> Call: +14734091240 </a> </h5>
+					    </div> '''
+       html3 = f'''
+                                        </div>
 				</article> 
 			</div>	
-			
 '''
        html0 = html1 + html2 + html3
        html += html0
