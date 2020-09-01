aab2975f69a0b4aafd453cfba50aa92b1197cdf7
jtkrohm <59145535+jtkrohm@users.noreply.github.com>
2020-09-01 17:24:11 -0400

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,5 +1,5 @@
 document.getElementById("cart-eggs").addEventListener("click", function(){
   localStorage.setItem("eggs-tray", "1");
-  alert(localStorage.getItem("eggs-tray"));
+  console.log(localStorage.getItem("eggs-tray"));
 });
 
