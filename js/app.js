86f327b82e0b66d86b43e45b2d475beecadf75a2
jtkrohm <59145535+jtkrohm@users.noreply.github.com>
2020-10-14 18:49:09 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //starts at 01:55:02
 //you may not need closeCartBtn, cartDOM, cartOverlay 
-
+//the "products-center" class is used for all articles so you need to make another class for the actual products
 //variables
 
 const cartBtn = document.querySelector(".cart-btn");
@@ -10,4 +10,7 @@ const cartDOM = document.querySelector(".cart");
 const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
-const cartContent = 
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+const btns = document.querySelector(".bag-btn");
+
