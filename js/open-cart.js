d3f9c0aeddc4c6a33f1ec9390f58847e4aa2a929
jtkrohm <59145535+jtkrohm@users.noreply.github.com>
2020-10-14 16:56:42 -0400

    Update open-cart.js

--- js/open-cart.js
+++ js/open-cart.js
@@ -1,4 +1,8 @@
-openCart() {
+closeCart() {
   document.getElementById('cart0').style.visibility='hidden';
-   document.getElementById('cart1').style.transform='translateX(-100vw)';
+  document.getElementById('cart1').style.transform='translateX(-100vw)';
+}
+openCart() {
+  document.getElementById('cart0').style.visibility='visible';
+  document.getElementById('cart1').style.transform='translateX(0)';
 }
