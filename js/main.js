d01bab825f1b42f1fce44f98610f1d8a828d29a2
jtkrohm <59145535+jtkrohm@users.noreply.github.com>
2020-10-14 21:36:30 -0400

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const thisYear = new Date().getFullYear();
+
+ //  document.write(new Date().getFullYear());
+
+
+ //  </span> migone.cc </p>
+
+//querySelect or getElement by id the footer
+// setup up text in a function
+//innerhtml ,innertext? , or document write it to screen
