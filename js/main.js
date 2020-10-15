8eaeea597ba8a20220e055c44d00ee4f162cfd26
jtkrohm <59145535+jtkrohm@users.noreply.github.com>
2020-10-14 22:37:40 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,3 +22,13 @@ document.getElementById('page-footer').innerHTML= `
                 <p> <a href="/html/terms.html">Terms and Agreements </a> </p>
  
 ` ;
+
+function closeCart() {
+  document.getElementById('cart0').style.visibility="hidden";
+  document.getElementById('cart1').style.transform="translateX(-100vw)";
+}
+
+function openCart() {
+  document.getElementById('cart0').style.visibility="visible";
+  document.getElementById('cart1').style.transform="translateX(0)";
+}
