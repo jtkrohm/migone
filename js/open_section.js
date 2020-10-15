4ec1d3a4d74d47da14989ffc25b5740b515bd5db
jtkrohm <59145535+jtkrohm@users.noreply.github.com>
2020-10-14 23:21:23 -0400

    Update open_section.js

--- js/open_section.js
+++ js/open_section.js
@@ -1,22 +1,26 @@
-//clock
+//core js file
+//modular js file
+//page specific js file ##
 
-function sectionTime() {
-  var openHourTitle = ' Open Today '; 
-  var today = new Date();
-  var h = today.getHours();
-  var hFinal  = h;
-  var amPm = "am";
-  if (h > 12) { hFinal = h - 12};
-  if (h  > 11) { amPm  = "pm" };
+  const openHourTag = document.getElementById('open-hours');
+  const openHourTitle = ' Open Today '; 
+  let today = new Date();
+  let h = today.getHours();
+  let hFinal  = h;
+  const amPm = "am";
   var m = today.getMinutes();
   var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
+  let m = checkTime(m);
+  let s = checkTime(s);
+
+function sectionTime() {
+  if (h > 12) { hFinal = h - 12};
+  if (h  > 11) { amPm  = "pm" };
   if (h > 16) {openHourTitle = 'Closing Soon'} ;
   if (h < 10) {openHourTitle = 'Opening Soon'} ;
-  document.getElementById('open-hours').innerHTML =
-  openHourTitle; 
+  openHourTag.innerHTML = openHourTitle; 
 }
+
 function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
