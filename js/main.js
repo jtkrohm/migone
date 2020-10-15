26bc2e2d7bff1bda613f68b0be65e25f8b6ed170
jtkrohm <59145535+jtkrohm@users.noreply.github.com>
2020-10-14 22:30:38 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,4 +9,17 @@ document.getElementById('page-footer').innerHTML= `
 		<br>
 		<hr>
                 <p> Copyright &copy; <span> ${thisYear} </span> migone.cc </p>
+
+                <a href="https://www.instagram.com/migone.cc"> <i class="fab fa-instagram" id="footer-icon"> </i> </a>
+
+                <i class="fab fa-facebook" id="footer-icon"></i> 
+
+                <a href="https://api.whatsapp.com/send?phone=14734044438&text=example"> <i class="fab fa-whatsapp" id="footer-icon"> </i> </a>
+
+                <p> <a href="/html/privacy.html">Privacy </a> </p>
+
+                <p> <a href="/html/contact.html">Contact </a> </p>
+  
+                <p> <a href="/html/terms.html">Terms and Agreements </a> </p>
+ 
 ` ;
