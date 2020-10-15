0bac3984a7e1483c6d6bb56df50028459d26926c
jtkrohm <59145535+jtkrohm@users.noreply.github.com>
2020-10-14 23:30:20 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 //core js file
 //modular js file
 //page specific js file ##
-
+  const clockTag = document.getElementById('clock');
   const openHourTag = document.getElementById('open-hours');
   const openHourTitle = ' Open Today '; 
   let today = new Date();
   let h = today.getHours();
   let hFinal  = h;
   const amPm = "am";
-  var m = today.getMinutes();
-  var s = today.getSeconds();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
   let m = checkTime(m);
   let s = checkTime(s);
 
@@ -24,4 +24,14 @@ function sectionTime() {
 function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
+}
+//clock
+
+function startTime() {
+  if (h > 12) { hFinal = h - 12};
+  if (h  > 11) { amPm  = "pm" };
+  m = checkTime(m);
+  s = checkTime(s);
+  clockTag.innerHTML =  hFinal + ":" + m + ":" + s + " " + amPm; 
+  var t = setTimeout(startTime, 500);
 }
