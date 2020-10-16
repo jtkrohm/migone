139c63cbe5db365971695e7d5f3a1f6127f34227
jtkrohm <59145535+jtkrohm@users.noreply.github.com>
2020-10-16 10:28:33 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -18,29 +18,17 @@ function sectionTime() {
   m = checkTime(m);
   s = checkTime(s);
   openHourTag.innerHTML = openHourTitle; 
+  if (h > 12) { hFinal = h - 12};
+  if (h  > 11) { amPm  = "pm" };
+  clockTag.innerHTML = hFinal + ":" + m + ":" + s + " " + amPm; 
+  var t = setTimeout(startTime, 500);
 }
 
 function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
 }
-//clock
 
-function startTime() {
-  var today = new Date();
-  var h = today.getHours();
-  var hFinal  = h;
-  var amPm = "am";
-  if (h > 12) { hFinal = h - 12};
-  if (h  > 11) { amPm  = "pm" };
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  document.getElementById('clock').innerHTML =
-  hFinal + ":" + m + ":" + s + " " + amPm; 
-  var t = setTimeout(startTime, 500);
-}
 
 sectionTime();
-startTime();
+
