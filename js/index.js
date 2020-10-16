4b5bb84e2e5e0c2ff9f18d2f9946230e859abc0a
jtkrohm <59145535+jtkrohm@users.noreply.github.com>
2020-10-16 19:12:55 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,32 +6,34 @@ const clockTag = document.getElementById('clock');
 const openHourTag = document.getElementById('open-hours');
 var openHourTitle = ' Open Today '; 
 
-function sectionTime() {
-  var today = new Date();
-  var h = today.getHours();
-  var hFinal  = h;
-  var amPm = "am";
-  if (h > 12) { hFinal = h - 12};
-  if (h  > 11) { amPm  = "pm" };
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  clockTag.innerHTML =
-  hFinal + ":" + m + ":" + s + " " + amPm; 
-  var t = setTimeout(sectionTime, 500);
+document.addEventListener('DOMContentLoaded', function() {
+    // IIFE 1
+  (function() {
+    var today = new Date();
+    var h = today.getHours();
+    var hFinal  = h;
+    var amPm = "am";
+    if (h > 12) { hFinal = h - 12};
+    if (h  > 11) { amPm  = "pm" };
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    clockTag.innerHTML =
+    hFinal + ":" + m + ":" + s + " " + amPm; 
+    var t = setTimeout(sectionTime, 500);
 
-  var today = new Date();
-  var h = today.getHours();
-  var hFinal  = h;
-  var amPm = "am";
-  if (h > 16) {openHourTitle = 'Closing Soon'} ;
-  if (h < 10) {openHourTitle = 'Opening Soon'} ;
-  var m = today.getMinutes();
-  var s = today.getSeconds();
-  m = checkTime(m);
-  s = checkTime(s);
-  openHourTag.innerHTML = openHourTitle; 
+    var today = new Date();
+    var h = today.getHours();
+    var hFinal  = h;
+    var amPm = "am";
+    if (h > 16) {openHourTitle = 'Closing Soon'} ;
+    if (h < 10) {openHourTitle = 'Opening Soon'} ;
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    openHourTag.innerHTML = openHourTitle; 
 }
 
 function checkTime(i) {
@@ -40,5 +42,4 @@ function checkTime(i) {
 }
 
 
-sectionTime();
 
