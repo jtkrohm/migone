dde967426863461db0822b6ab1701d1edb7fc901
jtkrohm <59145535+jtkrohm@users.noreply.github.com>
2021-05-24 10:33:58 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -26,8 +26,8 @@ function sectionTime() {
     var h = today.getHours();
     var hFinal  = h;
     var amPm = "am";
-    if (h > 16) {openHourTitle = 'Closing Soon'} ;
-    if (h < 10) {openHourTitle = 'Opening Soon'} ;
+   // if (h > 16) {openHourTitle = 'Closing Soon'} ;
+   // if (h < 10) {openHourTitle = 'Opening Soon'} ;
     var m = today.getMinutes();
     var s = today.getSeconds();
     m = checkTime(m);
